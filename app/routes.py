36ca9dc6fd561af99bb2d068b95e36b6befb920e
Eric Zielonka <zielonka.eric@gmail.com>
2019-03-08 14:30:27 -0500

    designed and built more of the api skeleton

--- app/routes.py
+++ app/routes.py
@@ -16,31 +16,66 @@ def help():
 def help_adv():
     return render_template('help.txt', algorithms=Algorithm, adv=True)
 
+
 @app.route('/api/model', methods=['POST'])
 def model_create():
+    # returns the model_id
     if request.mimetype == 'multipart/form-data':
         #create a model
         data = request.form
         files = request.files
 
-        return str(data.get("data")) + " " + request.mimetype
+        return str(data) + " " + request.mimetype
     else:
         pass #error
 
-@app.route('/api/model/<int:id>', methods=['GET'])
-def model_status(id):
+@app.route('/api/model/<int:model_id>', methods=['GET'])
+def model_status(model_id):
+    # returns the creation status
     pass
 
-@app.route('/api/model/<int:id>/download', methods=['GET'])
-def model_download(id):
-    pickledModel = b'hello world\n'
+@app.route('/api/model/<int:model_id>/download', methods=['GET'])
+def model_download(model_id):
+    # returns the pickled model
+    pickled_model = b'hello world\n'
 
     return send_file(
-        io.BytesIO(pickledModel),
+        io.BytesIO(pickled_model),
         mimetype='application/octet-stream',
         as_attachment=True,
         attachment_filename='%s.txt' % id)     #change filename
 
-@app.route('/api/model/<int:id>/stats', methods=['GET'])
-def model_stats(id):
+@app.route('/api/model/<int:model_id>/test', methods=['POST'])
+def model_test(model_id):
+    # returns the new result_id
+    pass
+
+@app.route('/api/model/<int:model_id>/results', methods=['GET'])
+def model_results(model_id):
+    # returns all of model's testing results
+    pass
+
+@app.route('/api/model/<int:model_id>/remove', methods=['POST'])
+def model_remove(model_id):
+    # returns a confirmation that the model was removed
+    pass
+
+@app.route('/api/results/<int:result_id>', methods=['GET'])
+def get_results(result_id):
+    # returns one set of results
+    pass
+
+@app.route('/api/data', methods=['POST'])
+def upload_data():
+    # returns new data_id
+    pass
+
+@app.route('/api/data/<int:data_id>', methods=['GET'])
+def data_get(data_id):
+    # returns the data file
+    pass
+
+@app.route('/api/data/<int:data_id>/remove', methods=['POST'])
+def data_remove(data_id):
+    # returns a confirmation that the data was removed
     pass
